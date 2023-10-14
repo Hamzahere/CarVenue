0fc56b3ea010db30c60e2e1c1ab9608bcffdae13
hamza-ali-99 <hamza@eikeconsulting.com>
2023-10-14 19:05:51 +0500

    Combobox search filter UI added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { Hero } from "@/components";
+import { SearchBar, CustomFilter, Hero } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="home__text-container">
+        <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+        <p>Explore out cars you might like</p>
+      </div>
+      <div className="home__filters">
+        <SearchBar />
+      </div>
     </main>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Footer, NavBar };
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  NavBar,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
